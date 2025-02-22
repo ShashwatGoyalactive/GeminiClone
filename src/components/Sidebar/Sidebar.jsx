44ebaddcf49930a22eeeeaa5cce8d8067f133fa6
Shashwat <shashwatgoyal2004@gmail.com>
2025-02-22 23:04:13 +0530

    Gemini Flash 2.0 model  working clone frontend

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,58 @@
+import './Sidebar.css';
+
+import {assets} from '../../assets/assets'
+import { useContext, useState } from 'react';
+import { Context } from '../../context/Context';
+
+const Sidebar = () => {
+    const[extended , setExtended] = useState(true)
+    const {onSent, previousPrompt, setRecentPrompt , newChat} = useContext(Context);
+
+ const loadPrompt = async(prompt) => {
+  setRecentPrompt(prompt);
+  await onSent(prompt);
+ }
+    return (
+        
+    <div className="sidebar">
+      <div className="top">
+        <img className="menu" src={assets.menu_icon} alt="" 
+        onClick={() => (setExtended(!extended))} />
+
+        <div onClick={() => newChat()} className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+        {extended?<p>New Chat</p> : null}  
+        </div>
+
+        {extended?<div className="recent">
+          <p className="recent-title">Recent</p>
+          { previousPrompt.map((item , index) =>{
+            return (
+              <div onClick={()=> loadPrompt(item)} className="recent_entry" key={index}>
+            <img src={assets.message_icon} alt="" />
+            <p>{item.slice(0,18)}...</p>
+          </div>
+            )
+          } )}          
+        </div>: null}
+        
+      </div>
+      <div className="bottom">
+        <div className="bottom_item recent_entry">
+          <img src={assets.question_icon} alt="" />
+         {extended? <p>Help</p> : null}
+        </div>
+        <div className="bottom_item recent_entry">
+          <img src={assets.history_icon} alt="" />
+          {extended?<p>History</p> : null}
+        </div>
+        <div className="bottom_item recent_entry">
+          <img src={assets.setting_icon} alt="" />
+        {extended?<p>Settings</p> : null}  
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar
